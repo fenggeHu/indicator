00128052fec7486806a562d007c219d994bfa0ee
hujinfeng <pbfoxhu@gmail.com>
2022-10-08 17:23:57 +0800

    update

--- src/main/java/strategy/ChartBar.java
+++ src/main/java/strategy/ChartBar.java
@@ -1,6 +1,7 @@
 package strategy;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Description: chart bar
@@ -8,11 +9,25 @@ import lombok.Data;
  * @author jinfeng.hu  @Date 2022-10-06
  **/
 @Data
+@NoArgsConstructor
 public class ChartBar {
     public String[] datetime;
     public double[] open;
     public double[] close;
     public double[] high;
     public double[] low;
+    // 成交量
     public long[] volume;
+    // 成交额
+    public long[] turnover;
+
+    public ChartBar(int size) {
+        this.datetime = new String[size];
+        this.open = new double[size];
+        this.close = new double[size];
+        this.high = new double[size];
+        this.low = new double[size];
+        this.volume = new long[size];
+        this.turnover = new long[size];
+    }
 }
