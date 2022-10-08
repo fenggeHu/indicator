cd58764cc5387f5a05eb4b8d630a83547fbb5a9f
hujinfeng <pbfoxhu@gmail.com>
2022-10-08 09:02:17 +0800

    完成TrendIndicators

--- src/main/java/indicator/TrendIndicators.java
+++ src/main/java/indicator/TrendIndicators.java
@@ -2,6 +2,7 @@ package indicator;
 
 import base.Tree;
 import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.Triple;
 
 import static indicator.Helper.*;
 
@@ -9,6 +10,8 @@ import static indicator.Helper.*;
  * @author jinfeng.hu  @Date 2022-10-07
  **/
 public class TrendIndicators {
+    private static final double psarAfStep = 0.02;
+    private static final double psarAfMax = 0.20;
 
     // The AbsolutePriceOscillator function calculates a technical indicator that is used
     // to follow trends. APO crossing above zero indicates bullish, while crossing below
@@ -130,6 +133,274 @@ public class TrendIndicators {
         return dema;
     }
 
+    // Exponential Moving Average (EMA).
+    public static double[] ema(int period, double[] values) {
+        double[] result = new double[values.length];
+
+        double k = 2.00 / (1 + period);
+        for (int i = 0; i < values.length; i++) {
+            if (i > 0) {
+                result[i] = (values[i] * k) + (result[i - 1] * (1 - k));
+            } else {
+                result[i] = values[i];
+            }
+        }
+
+        return result;
+    }
+
+    // Moving Average Convergence Divergence (MACD).
+    //
+    // MACD = 12-Period EMA - 26-Period EMA.
+    // Signal = 9-Period EMA of MACD.
+    //
+    // Returns MACD, signal.
+    public static Pair<double[], double[]> macd(double[] close) {
+        double[] ema12 = ema(12, close);
+        double[] ema26 = ema(26, close);
+        double[] macd = subtract(ema12, ema26);
+        double[] signal = ema(9, macd);
+
+        return Pair.of(macd, signal);
+    }
+
+    // The Mass Index (MI) uses the high-low range to identify trend reversals
+    // based on range expansions.
+    //
+    // Singe EMA = EMA(9, Highs - Lows)
+    // Double EMA = EMA(9, Single EMA)
+    // Ratio = Single EMA / Double EMA
+    // MI = Sum(25, Ratio)
+    //
+    // Returns mi.
+    public static double[] massIndex(double[] high, double[] low) {
+        double[] ema1 = ema(9, subtract(high, low));
+        double[] ema2 = ema(9, ema1);
+        double[] ratio = divide(ema1, ema2);
+        double[] mi = sum(25, ratio);
+
+        return mi;
+    }
+
+    // Moving Chande Forecast Oscillator calculates based on
+    // the given period.
+    //
+    // The Chande Forecast Oscillator developed by Tushar Chande The Forecast
+    // Oscillator plots the percentage difference between the closing price and
+    // the n-period linear regression forecasted price. The oscillator is above
+    // zero when the forecast price is greater than the closing price and less
+    // than zero if it is below.
+    //
+    // R = Linreg(Closing)
+    // CFO = ((Closing - R) / Closing) * 100
+    //
+    // Returns cfo.
+    public static double[] movingChandeForecastOscillator(int period, double[] closing) {
+        double[] x = generateNumbers(0, closing.length, 1);
+        double[] r = Regression.movingLinearRegressionUsingLeastSquare(period, x, closing);
+
+        double[] cfo = multiplyBy(divide(subtract(closing, r), closing), 100);
+
+        return cfo;
+    }
+
+    // Moving max for the given period.
+    public static double[] max(int period, double[] values) {
+        double[] result = new double[values.length];
+        double[] buffer = new double[period];
+        Tree bst = Tree.New();
+        for (int i = 0; i < values.length; i++) {
+            bst.insert(values[i]);
+
+            if (i >= period) {
+                bst.remove(buffer[i % period]);
+            }
+
+            buffer[i % period] = values[i];
+            result[i] = bst.max().doubleValue();
+        }
+
+        return result;
+    }
+
+    // Moving min for the given period.
+    public static double[] min(int period, double[] values) {
+        double[] result = new double[values.length];
+        double[] buffer = new double[period];
+        Tree bst = Tree.New();
+
+        for (int i = 0; i < values.length; i++) {
+            bst.insert(values[i]);
+
+            if (i >= period) {
+                bst.remove(buffer[i % period]);
+            }
+
+            buffer[i % period] = values[i];
+            result[i] = bst.min().doubleValue();
+        }
+
+        return result;
+    }
+
+    // Parabolic SAR. It is a popular technical indicator for identifying the trend
+    // and as a trailing stop.
+    //
+    // PSAR = PSAR[i - 1] - ((PSAR[i - 1] - EP) * AF)
+    //
+    // If the trend is Falling:
+    //  - PSAR is the maximum of PSAR or the previous two high values.
+    //  - If the current high is greather than or equals to PSAR, use EP.
+    //
+    // If the trend is Rising:
+    //  - PSAR is the minimum of PSAR or the previous two low values.
+    //  - If the current low is less than or equals to PSAR, use EP.
+    //
+    // If PSAR is greater than the closing, trend is falling, and the EP
+    // is set to the minimum of EP or the low.
+    //
+    // If PSAR is lower than or equals to the closing, trend is rising, and the EP
+    // is set to the maximum of EP or the high.
+    //
+    // If the trend is the same, and AF is less than 0.20, increment it by 0.02.
+    // If the trend is not the same, set AF to 0.02.
+    //
+    // Based on video https://www.youtube.com/watch?v=MuEpGBAH7pw&t=0s.
+    //
+    // Returns psar, trend
+    public static Pair<double[], Trend[]> ParabolicSar(double[] high, double[] low, double[] closing) {
+        checkSameSize(high, low);
+
+        Trend[] trend = new Trend[high.length];
+        double[] psar = new double[high.length];
+
+        double af, ep;
+
+        trend[0] = Trend.Falling;
+        psar[0] = high[0];
+        af = psarAfStep;
+        ep = low[0];
+
+        for (int i = 1;
+             i < psar.length;
+             i++) {
+            psar[i] = psar[i - 1] - ((psar[i - 1] - ep) * af);
+
+            if (trend[i - 1] == Trend.Falling) {
+                psar[i] = Math.max(psar[i], high[i - 1]);
+                if (i > 1) {
+                    psar[i] = Math.max(psar[i], high[i - 2]);
+                }
+
+                if (high[i] >= psar[i]) {
+                    psar[i] = ep;
+                }
+            } else {
+                psar[i] = Math.min(psar[i], low[i - 1]);
+                if (i > 1) {
+                    psar[i] = Math.min(psar[i], low[i - 2]);
+                }
+
+                if (low[i] <= psar[i]) {
+                    psar[i] = ep;
+                }
+            }
+
+            double prevEp = ep;
+
+            if (psar[i] > closing[i]) {
+                trend[i] = Trend.Falling;
+                ep = Math.min(ep, low[i]);
+            } else {
+                trend[i] = Trend.Rising;
+                ep = Math.max(ep, high[i]);
+            }
+
+            if (trend[i] != trend[i - 1]) {
+                af = psarAfStep;
+            } else if (prevEp != ep && af < psarAfMax) {
+                af += psarAfStep;
+            }
+        }
+
+        return Pair.of(psar, trend);
+    }
+
+    // The Qstick function calculates the ratio of recent up and down bars.
+    //
+    // QS = Sma(Closing - Opening)
+    //
+    // Returns qs.
+    public static double[] qstick(int period, double[] opening, double[] closing) {
+        double[] qs = sma(period, subtract(closing, opening));
+        return qs;
+    }
+
+    // The Kdj function calculates the KDJ  indicator, also known as
+    // the Random Index. KDJ is calculated similar to the Stochastic
+    // Oscillator with the difference of having the J line. It is
+    // used to analyze the trend and entry points.
+    //
+    // The K and D lines show if the asset is overbought when they
+    // crosses above 80%, and oversold when they crosses below
+    // 20%. The J line represents the divergence.
+    //
+    //
+    // RSV = ((Closing - Min(Low, rPeriod))
+    //       / (Max(High, rPeriod) - Min(Low, rPeriod))) * 100
+    // K = Sma(RSV, kPeriod)
+    // D = Sma(K, dPeriod)
+    // J = (3 * K) - (2 * D)
+    //
+    // Returns k, d, j.
+    public static Triple<double[], double[], double[]> kdj(int rPeriod, int kPeriod, int dPeriod,
+                                                           double[] high, double[] low, double[] closing) {
+        double[] highest = max(rPeriod, high);
+        double[] lowest = min(rPeriod, low);
+
+        double[] rsv = multiplyBy(divide(subtract(closing, lowest), subtract(highest, lowest)), 100);
+
+        double[] k = sma(kPeriod, rsv);
+        double[] d = sma(dPeriod, k);
+        double[] j = subtract(multiplyBy(k, 3), multiplyBy(d, 2));
+
+        return Triple.of(k, d, j);
+    }
+
+    // The DefaultKdj function calculates KDJ based on default periods
+    // consisting of rPeriod of 9, kPeriod of 3, and dPeriod of 3.
+    //
+    // Returns k, d, j.
+    public static Triple<double[], double[], double[]> defaultKdj(double[] high, double[] low, double[] closing) {
+        return kdj(9, 3, 3, high, low, closing);
+    }
+
+    // Rolling Moving Average (RMA).
+    //
+    // R[0] to R[p-1] is SMA(values)
+    // R[p] and after is R[i] = ((R[i-1]*(p-1)) + v[i]) / p
+    //
+    // Returns r.
+    public static double[] rma(int period, double[] values) {
+        double[] result = new double[values.length];
+        double sum = 0.00;
+
+        for (int i = 0; i < values.length; i++) {
+            int count = i + 1;
+
+            if (i < period) {
+                sum += values[i];
+            } else {
+                sum = result[i - 1] * (period - 1) + values[i];
+                count = period;
+            }
+
+            result[i] = sum / count;
+        }
+
+        return result;
+    }
+
     // Simple Moving Average (SMA).
     public static double[] sma(int period, double[] values) {
         double[] result = new double[values.length];
@@ -168,76 +439,6 @@ public class TrendIndicators {
             }
 
             result[i] = sinceLast;
-        }
-
-        return result;
-    }
-
-    // Exponential Moving Average (EMA).
-    public static double[] ema(int period, double[] values) {
-        double[] result = new double[values.length];
-
-        double k = 2.00 / (1 + period);
-        for (int i = 0; i < values.length; i++) {
-            if (i > 0) {
-                result[i] = (values[i] * k) + (result[i - 1] * (1 - k));
-            } else {
-                result[i] = values[i];
-            }
-        }
-
-        return result;
-    }
-
-    // Moving Average Convergence Divergence (MACD).
-    //
-    // MACD = 12-Period EMA - 26-Period EMA.
-    // Signal = 9-Period EMA of MACD.
-    //
-    // Returns MACD, signal.
-    public static Pair<double[], double[]> macd(double[] close) {
-        double[] ema12 = ema(12, close);
-        double[] ema26 = ema(26, close);
-        double[] macd = subtract(ema12, ema26);
-        double[] signal = ema(9, macd);
-
-        return Pair.of(macd, signal);
-    }
-
-    // Moving max for the given period.
-    public static double[] max(int period, double[] values) {
-        double[] result = new double[values.length];
-        double[] buffer = new double[period];
-        Tree bst = Tree.New();
-        for (int i = 0; i < values.length; i++) {
-            bst.insert(values[i]);
-
-            if (i >= period) {
-                bst.remove(buffer[i % period]);
-            }
-
-            buffer[i % period] = values[i];
-            result[i] = bst.max().doubleValue();
-        }
-
-        return result;
-    }
-
-    // Moving min for the given period.
-    public static double[] min(int period, double[] values) {
-        double[] result = new double[values.length];
-        double[] buffer = new double[period];
-        Tree bst = Tree.New();
-
-        for (int i = 0; i < values.length; i++) {
-            bst.insert(values[i]);
-
-            if (i >= period) {
-                bst.remove(buffer[i % period]);
-            }
-
-            buffer[i % period] = values[i];
-            result[i] = bst.min().doubleValue();
         }
 
         return result;
@@ -285,7 +486,7 @@ public class TrendIndicators {
     //   TRIMA = SMA((period + 1) / 2, SMA((period + 1) / 2, values))
     //
     // Returns trima.
-    public static double[] Trima(int period, double[] values) {
+    public static double[] trima(int period, double[] values) {
         int n1, n2;
 
         if (period % 2 == 0) {
