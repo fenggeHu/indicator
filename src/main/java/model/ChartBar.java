a28d35f408bbd16bc8e0a4d436f45e80b6101ce8
hujinfeng <pbfoxhu@gmail.com>
2022-10-10 14:32:05 +0800

    完成本地文件数据读写测试

--- src/main/java/model/ChartBar.java
+++ src/main/java/model/ChartBar.java
@@ -13,17 +13,32 @@ import lombok.NoArgsConstructor;
 public class ChartBar {
     public String[] datetime;
     public double[] open;
-    public double[] close;
     public double[] high;
     public double[] low;
+    public double[] close;
     public long[] volume;
 
     public ChartBar(int size) {
         this.datetime = new String[size];
         this.open = new double[size];
-        this.close = new double[size];
         this.high = new double[size];
         this.low = new double[size];
+        this.close = new double[size];
         this.volume = new long[size];
+    }
+
+    // date,ohlcv
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < this.getDatetime().length; i++) {
+            sb.append(this.getDatetime()[i]).append(",")
+                    .append(this.getOpen()[i]).append(",")
+                    .append(this.getHigh()[i]).append(",")
+                    .append(this.getLow()[i]).append(",")
+                    .append(this.getClose()[i]).append(",")
+                    .append(this.getVolume()[i]).append("\n");
+        }
+        return sb.toString();
     }
 }
