ca705bdb5e58db6f7d47a2d5532202e4be2fd713
hujinfeng <pbfoxhu@gmail.com>
2022-10-10 09:04:17 +0800

    read from local file

---
+++ src/main/java/model/Bar.java
@@ -0,0 +1,16 @@
+package model;
+
+import lombok.Data;
+
+/**
+ * @author jinfeng.hu  @Date 2022/10/10
+ **/
+@Data
+public class Bar {
+    public String datetime;
+    public double open;
+    public double close;
+    public double high;
+    public double low;
+    public long volume;
+}
